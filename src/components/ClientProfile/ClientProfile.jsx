4597009a7166f83363ec18ac894b2ba263321268
chinhuangyii <chinhuang_yii@xpeto.com>
2026-02-26 13:24:14 +0530

    revert

--- src/components/ClientProfile/ClientProfile.jsx
+++ src/components/ClientProfile/ClientProfile.jsx
@@ -19,8 +19,6 @@ const Convert = () => {
   const musicChannel = import.meta.env.VITE_LIVE_MUSIC;
   const sportsChannel = import.meta.env.VITE_LIVE_SPORTS;
   let origin = import.meta.env.VITE_TEST_IFRAME_ORIGIN;
-  console.log('ss');
-  console.log(botToken);
   const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
   
 
